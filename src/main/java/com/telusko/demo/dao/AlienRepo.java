bf8c6d17b613abc61de1d9600d39052fae4472a5
Qihao He <qi.he@maine.edu>
2019-12-29 21:55:56 -0500

    data jpa mvc h2 rest example 2

--- src/main/java/com/telusko/demo/dao/AlienRepo.java
+++ src/main/java/com/telusko/demo/dao/AlienRepo.java
@@ -8,8 +8,8 @@ import org.springframework.data.repository.CrudRepository;
 import com.telusko.demo.model.Alien;
 
 public interface AlienRepo extends CrudRepository<Alien, Integer>{
-	List<Alien> findByTech(String tech);
-	List<Alien> findByAidGreaterThan(int aid);
-	@Query("from Alien where tech=?1 order by aname")
-	List<Alien> findByTechSorted(String tech);
+//	List<Alien> findByTech(String tech);
+//	List<Alien> findByAidGreaterThan(int aid);
+//	@Query("from Alien where tech=?1 order by aname")
+//	List<Alien> findByTechSorted(String tech);
 }
