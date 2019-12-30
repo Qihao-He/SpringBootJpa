49d87b8c17cd7e5e57c30bf8698af19cd2ae180f
Qihao He <qi.he@maine.edu>
2019-12-29 23:10:50 -0500

    spring boot mvc rest post example

--- src/main/java/com/telusko/demo/controller/AlienController.java
+++ src/main/java/com/telusko/demo/controller/AlienController.java
@@ -5,16 +5,21 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.telusko.demo.dao.AlienRepo;
 import com.telusko.demo.model.Alien;
 
-@Controller
+//@Controller
+@RestController
 public class AlienController {
 	@Autowired
 	AlienRepo repo;
@@ -24,15 +29,20 @@ public class AlienController {
 		return "home.jsp";
 	}
 	
-	@RequestMapping("/addAlien")
-	public String addAlien(Alien alien) {
-		repo.save(alien);
-		return "home.jsp";
-	}
+//	@RequestMapping("/addAlien")
+//	public String addAlien(Alien alien) {
+//		repo.save(alien);
+//		return "home.jsp";
+//	}
 	
-//	@RequestMapping(path="/aliens", produces= {"application/xml"})
-	@RequestMapping(path="/aliens")
-	@ResponseBody
+	@PostMapping(path="/alien", consumes= {"application/json"})
+	public Alien addAlien(@RequestBody Alien alien) {
+		repo.save(alien);
+		return alien;
+	}
+//	@GetMapping(path="/aliens", produces= {"application/xml"})
+	@GetMapping(path="/aliens")
+//	@ResponseBody
 	public List<Alien> getAliens() {
 //		ModelAndView mv = new ModelAndView("showAlien.jsp");
 //		Alien alien = repo.findById(aid).orElse(new Alien());
@@ -45,7 +55,7 @@ public class AlienController {
 	}
 	
 	@RequestMapping("/alien/{aid}")
-	@ResponseBody
+//	@ResponseBody
 	public Optional<Alien> getAlien(@PathVariable("aid") int aid) {
 		return repo.findById(aid);
 	}
